dad4f6b4c0db13be28527673b1ee6d4186ef3130
Doug <douganderson444@gmail.com>
2021-10-28 19:26:59 -0300

    add demo

---
+++ demo/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { onMount } from 'svelte';
+	import Portal from '../../../../iframe-wallet/src/lib/Portal.svelte';
+</script>
+
+<h1>iFrame Wallet Demo</h1>
+
+<Portal origin={'http://localhost:3444'} />
