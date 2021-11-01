f5a10fe5545b71103c494cf76bc301ada5db803e
Doug <douganderson444@gmail.com>
2021-11-01 14:14:47 -0300

    rm redundant code

--- src/lib/Portal.svelte
+++ src/lib/Portal.svelte
@@ -13,7 +13,7 @@
 
 	let mounted;
 
-	// Wait for the iframe to load
+	// Wait for the iframe to load, then configure it
 	export const handleLoad = async () => {
 		config({ iframe, origin });
 	};
@@ -37,55 +37,14 @@
 	});
 
 	export const portal = {
-		CONSTANTS, // re-export for convenience
-		connect: async () => {
-			// @ts-ignore
-			let reply = await remote.connect();
-			return reply;
-		},
-
-		disconnect: async () => {
-			// @ts-ignore
-			let reply = await remote.disconnect();
-			return reply;
-		},
-
-		generateEd25519Keypair: async () => {
-			// @ts-ignore
-			const keypair = await remote.generate_ed25519_keypair();
-			return keypair;
-		},
-
-		newProxcryptor: async (name, secretKey) => {
-			// @ts-ignore
-			const pre = await remote.newProxcryptor(name, new Uint8Array(secretKey));
-			return pre;
-		},
-
-		selfEncrypt: async (name, data, tag) => {
-			// @ts-ignore
-			const em = await remote.selfEncrypt(name, data, tag);
-			return em;
-		},
-
-		generateReKey: async (sourcePreName, targetPK, tag) => {
-			// @ts-ignore
-			const reKey = await remote.generateReKey(sourcePreName, targetPK, tag);
-			return reKey;
-		},
-
-		reEncrypt: async (targetPK, encrypted_message, re_key) => {
-			// @ts-ignore
-			const rem = await remote.reEncrypt(targetPK, encrypted_message, re_key);
-			return rem;
-		},
-
-		reDecrypt: async (pre_name, re_encrypted_message) => {
-			// @ts-ignore
-			const decrypted = await remote.reDecrypt(pre_name, re_encrypted_message);
-			let textDecoder = new TextDecoder();
-			return textDecoder.decode(new Uint8Array(decrypted));
-		}
+		// inherit all super from remote handlers
+		...remote,
+		/**
+		 * Optional: If you want to extend the inherited functions from handlers,
+		 * such as additional name (ie. in camelCase) or change the output type (Uint8Array)
+		 * call await remote.<method> below and add your functionality
+		 */
+		CONSTANTS // re-export for convenience
 	};
 </script>
 
