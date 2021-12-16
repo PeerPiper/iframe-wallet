f98cd079bb742f167fe9a49e130df1b4ee660780
Doug <douganderson444@gmail.com>
2021-12-15 22:07:53 -0400

    cleanup demo

--- demo/src/routes/index.svelte
+++ demo/src/routes/index.svelte
@@ -2,14 +2,14 @@
 	import { onMount } from 'svelte';
 	import Portal from '../../../../iframe-wallet/src/lib/Portal.svelte';
 	import svg from '../../../../iframe-wallet/src/lib/graphics/mini-svg-bookmark.svg';
+
 	let mounted;
 	onMount(async () => {
 		mounted = true;
 	});
 	// Need these for Portal Component
 	let portal;
-	let portalLoaded = false;
-	let origin = 'http://localhost:3444'; // optional to change default
+	let origin = 'http://localhost:3444/'; // optional to change default
 
 	let connected = false;
 	let reply;
@@ -24,6 +24,8 @@
 
 	let data = 'some data here';
 	let tag = 'a tag for it';
+
+	$: portal && console.log({ portal });
 
 	const handleConnect = async () => {
 		// @ts-ignore
@@ -78,28 +80,28 @@
 	};
 </script>
 
-<img src={svg} alt="SolBlog" class="tab" />
+<!-- <img src={svg} alt="bookmark" class="tab" /> -->
 
 <h1>iFrame Wallet Portal Demo</h1>
 
 <p>
 	Wallet actions with to <b>{origin}</b> embedded in iFrame:
 	<br />
-	<button disabled={!portalLoaded || connected} on:click={handleConnect}>Connect</button><br />
-	<button disabled={!connected} on:click={handleDisconnect}>Disconnect</button><br />
-	<button disabled={!connected || alice_keypair} on:click={handleGenerateAlice}
+	<!-- <button disabled={!portal || connected} on:click={handleConnect}>Connect</button><br /> -->
+	<!-- <button disabled={!portal} on:click={handleDisconnect}>Disconnect</button><br /> -->
+	<!-- <button disabled={!portal || alice_keypair} on:click={handleGenerateAlice}
 		>Generate Alice Keypair</button
 	><br />
-	<button disabled={!connected || bob_keypair} on:click={handleGenerateBob}
+	<button disabled={!portal || bob_keypair} on:click={handleGenerateBob}
 		>Generate Bob Keypair</button
 	><br />
 	<button
-		disabled={!connected || !(alice_keypair && bob_keypair)}
+		disabled={!portal || !(alice_keypair && bob_keypair)}
 		on:click={() => handleNewProxcryptor('alice', alice_keypair.secretKey)}
 		>Create Alice Proxcryptor</button
 	><br />
 	<button
-		disabled={!connected || !(alice_keypair && bob_keypair)}
+		disabled={!portal || !(alice_keypair && bob_keypair)}
 		on:click={() => handleNewProxcryptor('bob', bob_keypair.secretKey)}
 		>Create Bob Proxcryptor</button
 	><br />
@@ -108,17 +110,17 @@
 	<button disabled={!pre} on:click={() => handleSelfDecrypt('alice', data, tag)}>SelfDecrypt</button
 	><br />
 	<button
-		disabled={!connected || !bob_keypair}
+		disabled={!portal || !bob_keypair}
 		on:click={() => handleGenerateReKey('alice', bob_keypair.publicKey, tag)}
 		>GenerateReKey for Alice or Proxy
 	</button><br />
 	<button
-		disabled={!connected || !bob_keypair}
+		disabled={!portal || !bob_keypair}
 		on:click={() => handleReEncrypt(bob_keypair.publicKey, em, reKey)}>ReEncrypt for Bob</button
 	><br />
-	<button disabled={!connected || !bob_keypair} on:click={() => handleReDecrypt('bob', rem)}
+	<button disabled={!portal || !bob_keypair} on:click={() => handleReDecrypt('bob', rem)}
 		>Bob Decrypt</button
-	>
+	> -->
 </p>
 
 {#if mounted}
@@ -126,8 +128,7 @@
 {/if}
 <br /><br />{reply?.status ? reply.status : ''}
 <br /><br />
-Loaded: {portalLoaded}<br />
-Connected: {connected}<br />
+Connected: {!!portal}<br />
 
 <style>
 	button {
